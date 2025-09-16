e40bfa6cd6e026d1355778f43ec7d3b9de27b769
flvffywvffy <marcus@marcusleonasharvey.com>
2025-09-16 18:01:27 +0100

    add 404 page

---
+++ 404.php
@@ -0,0 +1,10 @@
+<?php get_header(); ?>
+
+<article class="page-content">
+    <div class="container">
+        <h1>404 Not Found</h1>
+        <p>Apologies, but the page you requested could not be found.</p>
+    </div>
+</article>
+
+<?php get_footer(); ?>
