9ae0c9bb900dde04fe7250267e32868c7479e766
flvffywvffy <marcus@marcusleonasharvey.com>
2025-09-16 18:59:11 +0100

    update

--- js/script.js
+++ js/script.js
@@ -1,1 +1,9 @@
-console.log("Hello from script.js!");
+$(document).ready(function () {
+  const menu = $(".mobile-menu");
+  const openMenu = $(".navbar__mobile-btn");
+  openMenu.click(function () {
+    menu.toggleClass("active");
+    openMenu.toggleClass("active");
+    return false;
+  });
+});
