7b1b0fdff55f686fa0905bb1f84779e45be2d703
flvffywvffy <marcus@marcusleonasharvey.com>
2025-09-16 17:57:45 +0100

    add page.php

---
+++ page.php
@@ -0,0 +1,16 @@
+<?php get_header(); ?>
+
+<?php if (have_posts()) while (have_posts()) : the_post(); ?>
+
+
+    <?php include_once('includes/page-hero.php'); ?>
+
+    <article class="page--content">
+        <div class="container">
+            <?php the_content(); ?>
+        </div>
+    </article>
+
+<?php endwhile; ?>
+
+<?php get_footer(); ?>
