dd5e62f716883c095c93ad38d3b7e3201c2edd35
flvffywvffy <marcus@marcusleonasharvey.com>
2025-09-16 18:58:53 +0100

    update

--- header.php
+++ header.php
@@ -14,30 +14,38 @@
 
     <!-- Header -->
     <header class="navbar">
-        <div>
-            <a class="navbar__logo" href="<?php bloginfo('url'); ?>">
-                <?php
-                $custom_logo_id = get_theme_mod('custom_logo');
-                $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
-                if (has_custom_logo()) {
-                    echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
-                } else {
-                    echo '<h1>' . get_bloginfo('name') . '</h1>';
-                }
-                ?>
-            </a>
+        <div class="container">
+            <div class="navbar__left">
+                <a class="navbar__logo" href="<?php bloginfo('url'); ?>">
+                    <?php
+                    $custom_logo_id = get_theme_mod('custom_logo');
+                    $logo = wp_get_attachment_image_src($custom_logo_id, 'full');
+                    if (has_custom_logo()) {
+                        echo '<img src="' . esc_url($logo[0]) . '" alt="' . get_bloginfo('name') . '">';
+                    } else {
+                        echo '<h1>' . get_bloginfo('name') . '</h1>';
+                    }
+                    ?>
+                </a>
+            </div>
 
-            <button class="navbar__mobile-menu">
-                <i class="fas fa-bars"></i>
-                <i class="fas fa-times"></i>
-                <span class="accessibility">Menu</span>
-            </button>
+            <div class="navbar__right">
+                <button class="navbar__mobile-btn">
+                    <i class="fas fa-bars"></i>
+                    <i class="fas fa-times"></i>
+                    <span class="sr-only">Menu</span>
+                </button>
 
-            <nav class="navbar__links">
-                <?php wp_nav_menu(array('menu' => 'Main', 'container' => false)); ?>
-            </nav>
+                <nav class="navbar__links">
+                    <?php wp_nav_menu(array('menu' => 'Main', 'container' => false)); ?>
+                </nav>
+            </div>
         </div>
     </header>
+
+    <div class="mobile-menu">
+        <?php wp_nav_menu(array('menu' => 'Main', 'container' => false)); ?>
+    </div>
 
     <!-- Content -->
     <main id="main">
